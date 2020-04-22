5f085be0f6134be5f5d4a29299ef042e63c730b0
thanhlam1702 <thanhlam17021999@gmail.com>
2020-04-22 16:53:01 +0700

    edit login fb and gg

--- ShoesShelf/assets/js/save_js_test.js
+++ ShoesShelf/assets/js/save_js_test.js
@@ -12,3 +12,45 @@ var onFailure = function(error) {
 };
 //run sign in google
 initClient();
+
+
+
+
+// $('#menu-bars').on('click', function(){
+//   $('.right-menu').css('width','68%');
+// });
+// $('#toggle-close').on('click', function(){
+//   $('.right-menu').css('width','12%');
+// });
+//--------------------------------------------
+//--------------------------------------------
+// $('.search').hover('class', function(){
+//   var hasClass=$('.menu').hasClass('expanded');
+//   if(hasClass){
+//     $('.menu').toggleClass('expanded');
+//     $('.sub-menu').toggleClass('hidden');
+//     $('.container-close , .toggle').toggleClass('close-menu');
+//   }
+// });
+//-----------------------------------------------
+//script switch file css
+//-----------------------------------------------
+
+//-----------------------------------------------------
+// //load google
+// var initClient = function() {
+//     gapi.load('auth2', function() {
+//         //Retrieve the singleton for the GoogleAuth library and set up the
+//         //client.
+//         auth2 = gapi.auth2.init({
+//             client_id: '276514203511-rkdmf52kmh1ljdg29sjfj30fn1phubb3.apps.googleusercontent.com',
+//             cookiepolicy: 'single_host_origin',
+//             // Request scopes in addition to 'profile' and 'email'
+//             scope: 'profile email'
+//         }).then(function() {
+//             let auth3 = gapi.auth2.getAuthInstance();
+//             document.getElementById('user-name').innerHTML = auth3.currentUser.ie.Pt.Ad;
+//             document.getElementById('user-img').src = auth3.currentUser.ie.Pt.fL;
+//         })
+//     });
+// };
