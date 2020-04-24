664700b9a9bd14c31ce46638769923782eab4d91
thanhlam1702 <thanhlam17021999@gmail.com>
2020-04-24 19:33:05 +0700

    add wanna rock

--- ShoesShelf/assets/js/your-shelf.js
+++ ShoesShelf/assets/js/your-shelf.js
@@ -4,16 +4,22 @@ uploadImages = () => {
     var imgUpload = document.getElementById('upload-img');
     var reviewImg = document.getElementById('review-img-upload');
     reviewImg.innerHTML = '';
+    //run "for" all element have in "imgUpload",  later create element HTML assign value for this
     for( let i = 0; i < imgUpload.files.length; ++i){
+
         var reader = new FileReader();
 
         reader.onload = function (e) {
+            //create element HTML about:
+            //tag "img" have id name "img-upload"
+            //"src" = present result
+            //<img id="img-upload" src = e.target.result>
             var p = document.createElement('img');
             p.id = 'img-upload';
             p.src = e.target.result;
             reviewImg.appendChild(p);
         };
-    
+        //read the address of the ith element in  "imgUpload" 
         reader.readAsDataURL(imgUpload.files[i]);
     }
     
@@ -27,14 +33,18 @@ uploadImages = () => {
 //showup hidden content
 var chooseContentShoes = () => {
     const newShoes = document.getElementById('user-new-shoes');
+    const wannaRock = document.getElementById('user-wanner-rock');
     
     return {
+        //showup new shoes
         contentNewShoes: () => {
             newShoes.style.display = 'block';
+            wannaRock.style.display = 'none'
         },
-
+        //showup wanna rock
         contentWannaRock: () => {
             newShoes.style.display = 'none';
+            wannaRock.style.display = 'flex';
         }
     };
 }
