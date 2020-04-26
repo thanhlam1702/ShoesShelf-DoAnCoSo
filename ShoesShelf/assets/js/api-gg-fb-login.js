8f92f0b80f26ab049c14d30c1fed823d8f453037
AndieLe <le.andie195@gmail.com>
2020-04-26 17:12:26 +0700

    change newShoes theme and wanna rock

--- ShoesShelf/assets/js/api-gg-fb-login.js
+++ ShoesShelf/assets/js/api-gg-fb-login.js
@@ -2,11 +2,11 @@
 var statusLogin = -1;
 
 //scripts sign in with facebook
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+function statusChangeCallback(response) { // Called with the results from FB.getLoginStatus().
+    if (response.status === 'connected') { // Logged into your webpage and Facebook.
         statusLogin = 1;
         showUpSignInComplete();
-    } 
+    }
 }
 
 window.fbAsyncInit = function() {
@@ -38,11 +38,11 @@ function checkLoginState() { // Called when a person is finished with the Login 
 }
 
 //Initializes the Sign-In client.
-const  initClient =  function() {
-    gapi.load('auth2',async function() {
+const initClient = function() {
+    gapi.load('auth2', async function() {
         //Retrieve the singleton for the GoogleAuth library and set up the
         //client.
-        auth2 = await  gapi.auth2.init({
+        auth2 = await gapi.auth2.init({
             client_id: '276514203511-rkdmf52kmh1ljdg29sjfj30fn1phubb3.apps.googleusercontent.com',
             cookiepolicy: 'single_host_origin',
             // Request scopes in addition to 'profile' and 'email'
@@ -51,13 +51,13 @@ const  initClient =  function() {
         //wait google init load complete, after check status user login, if not login 
         //then isSingedIn return 'false', user login return true,
         //if 'true' load information  of user
-        if(auth2.isSignedIn.get() === true){
+        if (auth2.isSignedIn.get() === true) {
             onSuccess();
         }
 
         // Attach the click handler to the sign-in button
         const btnSignInGG = document.getElementsByClassName('customGPlusSignIn');
-        for(let i = 0; i < btnSignInGG.length; ++i){
+        for (let i = 0; i < btnSignInGG.length; ++i) {
             auth2.attachClickHandler(btnSignInGG[i], {}, onSuccess, onFailure);
         }
     });
@@ -66,7 +66,7 @@ const  initClient =  function() {
 const onSuccess = function() {
     statusLogin = 0;
     showUpSignInComplete();
- };
+};
 
 // Handle sign-in failures.
 const onFailure = function(error) {
@@ -77,23 +77,23 @@ initClient();
 
 //show layout after user login complete
 const showUpSignInComplete = () => {
-    document.getElementById('navbar-right-user').style.display = 'flex';
-    document.getElementById('left-menu').style.display = 'flex';
+        document.getElementById('navbar-right-user').style.display = 'flex';
+        document.getElementById('left-menu').style.display = 'flex';
 
-    document.getElementById('navbar-right').style.display = 'none';
-    document.getElementById('sign-in-modal').style.display = 'none';
-    document.getElementById('sign-up-modal').style.display = 'none';
+        document.getElementById('navbar-right').style.display = 'none';
+        document.getElementById('sign-in-modal').style.display = 'none';
+        document.getElementById('sign-up-modal').style.display = 'none';
 
-    showUser();
-    
-}
-//show information user about avatar, name...
+        showUser();
+
+    }
+    //show information user about avatar, name...
 const showUser = function() {
     const userName = document.getElementsByClassName('user-name');
     const userAvatar = document.getElementsByClassName('user-img');
     if (statusLogin === 1) {
         FB.api('/me?fields=name,picture.type(large),email', function(user_data) {
-            for(let i = 0; i < userName.length; ++i){
+            for (let i = 0; i < userName.length; ++i) {
                 userName[i].innerHTML = user_data.name;
                 userAvatar[i].src = user_data.picture.data.url;
             }
@@ -101,7 +101,7 @@ const showUser = function() {
         console.log('login fb');
     } else if (statusLogin === 0) {
         var auth2 = gapi.auth2.getAuthInstance();
-        for(let i = 0; i < userName.length; ++i){
+        for (let i = 0; i < userName.length; ++i) {
             userName[i].innerHTML = auth2.currentUser.get().getBasicProfile().getName();
             userAvatar[i].src = auth2.currentUser.get().getBasicProfile().getImageUrl();
         }
@@ -113,23 +113,21 @@ const showUser = function() {
 //logout facebook and google
 function logout() {
     if (statusLogin === 1) {
-        FB.logout(function(response) {
-        })
+        FB.logout(function(response) {})
         logoutComplete();
     } else if (statusLogin === 0) {
         var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function() {
-        });
+        auth2.signOut().then(function() {});
         logoutComplete();
     }
 }
 
-const logoutComplete = () =>{
+const logoutComplete = () => {
     document.getElementById('navbar-right-user').style.display = 'none';
     document.getElementById('left-menu').style.display = 'none';
 
     document.getElementById('navbar-right').style.display = 'flex';
     console.log('Log out Complete');
-    window.location = "https://shoesshelf.com/";
+    window.location = "localhost:5502/index.html/";
     statusLogin = -1;
 }
