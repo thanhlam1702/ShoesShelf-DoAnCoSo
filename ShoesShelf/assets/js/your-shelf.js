6a8a6e66cc3e6ee3c45fd234a3bdeb98e123d14c
thanhlam1702 <thanhlam17021999@gmail.com>
2020-05-11 12:00:00 +0700

    edit your-shelf.js

--- ShoesShelf/assets/js/your-shelf.js
+++ ShoesShelf/assets/js/your-shelf.js
@@ -1,5 +1,9 @@
+//make "your-shelf color white"
+document.getElementById('btn-your-shelf').style.color = 'white';
+
+
 //upload and review images 
-uploadImages = () => {
+function uploadImages() {
 
     const uploadImg = document.getElementById('upload-img');
     const reviewImg = document.getElementById('review-img-upload');
@@ -41,52 +45,140 @@ uploadImages = () => {
         document.getElementById('title-upload-img').style.display = 'none';
     }
 }
-//showup hidden content
-var chooseContentShoes = () => {
-    const userYourShelf = document.getElementById('user-your-shelf');
-    const userNewShoes = document.getElementById('user-new-shoes');
-    const userUpdate = document.getElementById('user-update');
-    const userWannaRock = document.getElementById('user-wanna-rock');
 
+//showup hidden content
+function chooseContentShoes() {
     const btnUserYourShelf = document.getElementById('btn-your-shelf');
     const btnUserNewShoes = document.getElementById('btn-new-shoes');
     const btnUserUpdate = document.getElementById('btn-update');
     const btnUserWannaRock = document.getElementById('btn-wanna-rock');
 
-    const colorManagerUser = '#4a525c';
+    const containerUserManager = document.getElementById('user-manager-shoes');
+
 
     return {
         //showup your shelf
         contentYourShelf: () => {
-            userNewShoes.style.display = userUpdate.style.display = userWannaRock.style.display = 'none';
-            userYourShelf.style.display = 'flex';
-
-            btnUserNewShoes.style.color = btnUserUpdate.style.color = btnUserWannaRock.style.color = colorManagerUser;
+            btnUserNewShoes.style.color = btnUserUpdate.style.color = btnUserWannaRock.style.color = '';
             btnUserYourShelf.style.color = 'white';
+
+            containerUserManager.innerHTML = `
+            <ul class="user-your-shelf" id="user-your-shelf">
+                <!-- card of images, short content of a post -->
+                <li class="cards-posts">
+                    <div class="card-container-img">
+                        <img src="./assets/images/test1.jpg" alt="" class="card-img">
+                    </div>
+                    <div class="card-container-content">
+                        <p class="card-short-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, hic. Harum necessitatibus, minima inventore consequatur numquam atque libero ipsam quis. Atque ad voluptatem earum ex id corporis quisquam. Saepe, magni!</p>
+                    </div>
+                </li>
+                <li class="cards-posts">
+                    <div class="card-container-img">
+                        <img src="./assets/images/test1.jpg" alt="" class="card-img">
+                    </div>
+                    <div class="card-container-content">
+                        <p class="card-short-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolor quas eius sit magnam cumque! Deserunt in cupiditate culpa illum doloribus iusto autem reprehenderit magni optio, expedita sapiente possimus eaque.</p>
+                    </div>
+                </li>
+            </ul>`;
         },
-        //showup new shoes
+        //show up content new shoes
         contentNewShoes: () => {
-            userYourShelf.style.display = userUpdate.style.display = userWannaRock.style.display = 'none';
-            userNewShoes.style.display = 'block';
-
-            btnUserYourShelf.style.color = btnUserUpdate.style.color = btnUserWannaRock.style.color = colorManagerUser;
+            btnUserYourShelf.style.color = btnUserUpdate.style.color = btnUserWannaRock.style.color = '';
             btnUserNewShoes.style.color = 'white';
-        },
-        //showup user update
-        contentUserUpdate: () => {
-            userYourShelf.style.display = userNewShoes.style.display = userWannaRock.style.display = 'none';
-            userUpdate.style.display = 'block';
 
-            btnUserYourShelf.style.color = btnUserNewShoes.style.color = btnUserWannaRock.style.color = colorManagerUser;
+            containerUserManager.innerHTML = `
+            <div class="user-new-shoes" id="user-new-shoes">
+                <!-- form contain button upload image -->
+                <form action="">
+                    <div class="container-upload-img">
+                        <!-- where img alter upload showup review -->
+                        <div class="review-img-upload" id="review-img-upload">
+                            <!-- <img src="" id="img-upload"> -->
+                            <label for="upload-img" class="btn-upload-img" >+</label>
+                        </div>
+                        <span class="title-upload-img" id="title-upload-img">Show us what you have...</span>
+                        <input type="file" name="img" accept="image/*" id="upload-img" onchange="uploadImages()" multiple>
+                    </div>
+                    <div class="container-editor">
+                        <textarea name="status" id="share-exp" placeholder="Your experiences..."></textarea>
+                    </div>
+                    <div class="container-choose-more">
+                        <select name="brands" class="brands" id="brands">
+                            <option selected hidden style="font-weight: bold;">Brands</option>
+                            <option value="vans">Vans</option>
+                            <option value="adidas">Adidas</option>
+                            <option value="nike">Nike</option>
+                            <option value="nike">Reebok</option>
+                            <option value="nike">Converse</option>
+                            <option value="nike">Puma</option>
+                            <option value="nike">Orther</option>
+                        </select>
+                        <input type="text" name="collections" placeholder="Collections" class="collections">
+                        <input type="text" name="hashtag" placeholder="#HashTag" class="hashtag">
+                    </div>
+                    <div class="container-btn-post">
+                        <button class="btn-post-status">Post</button>
+                    </div>
+                </form>
+            </div>`;
+        },
+
+        contentUserUpdate: () => {
+            btnUserYourShelf.style.color = btnUserNewShoes.style.color = btnUserWannaRock.style.color = '';
             btnUserUpdate.style.color = 'white';
+
+            containerUserManager.innerHTML = `
+            <div class="user-update" id="user-update">
+
+            </div>
+            `;
         },
         //showup wanna rock
         contentWannaRock: () => {
-            userYourShelf.style.display = userNewShoes.style.display = userUpdate.style.display = userWannaRock.style.display = 'none';
-            userWannaRock.style.display = 'flex';
-
-            btnUserYourShelf.style.color = btnUserNewShoes.style.color = btnUserUpdate.style.color = colorManagerUser;
+            btnUserYourShelf.style.color = btnUserNewShoes.style.color = btnUserUpdate.style.color = '';
             btnUserWannaRock.style.color = 'white';
+
+            containerUserManager.innerHTML = `
+            <ul class="user-wanna-rock" id="user-wanna-rock">
+                <!-- card contain img and name hashtag of post -->
+                <li class="card-wanna-rock">
+                    <div class="card-container-img">
+                        <img src="./assets/images/test1.jpg" alt="" class="card-img">
+                    </div>
+                    <div class="card-container-title">
+                        <p class="card-title">#Adidas #superStar #3trips</p>
+                    </div>
+                </li>
+
+                <li class="card-wanna-rock">
+                    <div class="card-container-img">
+                        <img src="./assets/images/test2.jpg" alt="" class="card-img">
+                    </div>
+                    <div class="card-container-title">
+                        <p class="card-title">#Converse #cdg #commeDesgarcons #black</p>
+                    </div>
+                </li>
+
+                <li class="card-wanna-rock">
+                    <div class="card-container-img">
+                        <img src="./assets/images/test3.jpg" alt="" class="card-img">
+                    </div>
+                    <div class="card-container-title">
+                        <p class="card-title">#Converse #cdg #commeDesgarcons #white</p>
+                    </div>
+                </li>
+
+                <li class="card-wanna-rock">
+                    <div class="card-container-img">
+                        <img src="./assets/images/test4.jpg" alt="" class="card-img">
+                    </div>
+                    <div class="card-container-title">
+                        <p class="card-title">#adidas #stansmith</p>
+                    </div>
+                </li>
+            </ul>`;
         }
     };
 }
