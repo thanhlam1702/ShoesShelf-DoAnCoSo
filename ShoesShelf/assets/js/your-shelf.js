fa5b7f6094f6eb3c86fe843a607fc93b6627c37e
thanhlam1702 <thanhlam17021999@gmail.com>
2020-06-02 03:35:08 +0700

    tach your-shelf ra tung trang, merge code back-end, chua merge font end

--- ShoesShelf/assets/js/your-shelf.js
+++ ShoesShelf/assets/js/your-shelf.js
@@ -1,5 +1,5 @@
 //make "your-shelf color white"
-document.getElementById('btn-your-shelf').style.color = 'white';
+
 
 
 //upload and review images 
@@ -44,85 +44,3 @@ function uploadImages() {
         document.getElementById('title-upload-img').style.display = 'none';
     }
 }
-
-//showup hidden content
-function chooseContentShoes() {
-    const btnUserYourShelf = document.getElementById('btn-your-shelf');
-    const btnUserNewShoes = document.getElementById('btn-new-shoes');
-    const btnUserUpdate = document.getElementById('btn-update');
-    // const btnUserWannaRock = document.getElementById('btn-wanna-rock');
-
-    const userYourShelf = document.getElementById('user-your-shelf');
-    const userNewShoes = document.getElementById('user-new-shoes');
-    const userUpdate = document.getElementById('user-update');
-    // const userWannaRock = document.getElementById('user-wanna-rock');
-
-    return {
-        //showup your shelf
-        contentYourShelf: () => {
-            userNewShoes.style.display = userUpdate.style.display = userWannaRock.style.display = 'none';
-            userYourShelf.style.display = 'flex';
-
-            btnUserNewShoes.style.color = btnUserUpdate.style.color = btnUserWannaRock.style.color = '';
-            btnUserYourShelf.style.color = 'white';
-        },
-        //show up content new shoes
-        contentNewShoes: () => {
-            userYourShelf.style.display = userUpdate.style.display = userWannaRock.style.display = 'none';
-            userNewShoes.style.display = 'block';
-
-            btnUserYourShelf.style.color = btnUserUpdate.style.color = btnUserWannaRock.style.color = '';
-            btnUserNewShoes.style.color = 'white';
-
-        },
-
-        contentUserUpdate: () => {
-            userYourShelf.style.display = userNewShoes.style.display = userWannaRock.style.display = 'none';
-            userUpdate.style.display = 'block';
-
-            btnUserYourShelf.style.color = btnUserNewShoes.style.color = btnUserWannaRock.style.color = '';
-            btnUserUpdate.style.color = 'white';
-
-        },
-        // //showup wanna rock
-        // contentWannaRock: () => {
-        //     userYourShelf.style.display = userNewShoes.style.display = userUpdate.style.display = userWannaRock.style.display = 'none';
-        //     userWannaRock.style.display = 'flex';
-
-        //     btnUserYourShelf.style.color = btnUserNewShoes.style.color = btnUserUpdate.style.color = '';
-        //     btnUserWannaRock.style.color = 'white';
-        // },
-    }
-}
-// function modalEditProfileUser() {
-//     var userContainer = document.getElementById('user-container');
-
-//     return {
-//         showUpModal: () => {
-//             var nodeModalEditUser = document.createElement('div');
-
-//             nodeModalEditUser.className = 'user-edit-profile';
-//             nodeModalEditUser.id = 'user-edit-profile';
-
-//             userContainer.appendChild(nodeModalEditUser);
-
-//             nodeModalEditUser.innerHTML = `
-//             <form class="modal-edit-profile">
-//                 <i class="close-modal-edit" onclick="modalEditProfileUser().closeModal()">x</i>
-//                 <div class="user-avatar">
-//                     <img src="" id="user-img" class="user-img">
-//                 </div>
-//                 <label for="change-avatar" class="btn-change-avatar">Change Photo</label>
-//                 <input type="file" name="img" accept="image/*" id="change-avatar">
-//                 <input type="text" value="" class="user-name" id="change-user-name">
-//                 <p>This could be your first name or a nickname <br> It's how you'll appear on ShoesShelf</p>
-//                 <button class="btn-save-change">Save Change</button>
-//             </form>
-//             `;
-//             showUser();
-//         },
-//         closeModal: () => {
-//             userContainer.removeChild(document.getElementById('user-edit-profile'));
-//         }
-//     }
-// }no
