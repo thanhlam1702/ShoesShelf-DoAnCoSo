69520d4c25b2d7f5634bed6cfc1a0dca826914d5
thanhlam1702 <thanhlam17021999@gmail.com>
2020-06-03 16:51:27 +0700

    edit css

--- ShoesShelf/models/Post.js
+++ ShoesShelf/models/Post.js
@@ -40,6 +40,26 @@ const Post = mongoose.model('Post', PostSchema);
 module.exports = Post;
 function getDate() {
     var d = new Date();
-    var fullTime = `${ d.getHours()}:${ d.getMinutes() } ${ d.getDate() } /${d.getMonth()}/${ d.getFullYear() }`;
+    var fullTime = `${getFormatTime()}  ${getFormatDate()} `;
     return fullTime.toString();
+
+    function getFormatTime(){
+        var hours = d.getHours().toString();
+        hours = hours.length > 1 ? hours : '0' + hours;
+
+        var minutes = d.getMinutes().toString();
+        minutes = minutes.length > 1 ? minutes : '0' + minutes;
+        return hours + ':' + minutes;
+    }
+    function getFormatDate() {
+        var year = d.getFullYear();
+
+        var month = (1 + d.getMonth()).toString();
+        month = month.length > 1 ? month : '0' + month;
+
+        var day = d.getDate().toString();
+        day = day.length > 1 ? day : '0' + day;
+
+        return day + '/' + month + '/' + year;
+    }
 }
